331b4c1dee3025452faffc1f72aab54f390980b8
Aldrin Castro <aldrin.castro@regovtech.com>
2020-10-13 16:37:37 +0800

    added decryption

--- src/pages/Password.tsx
+++ src/pages/Password.tsx
@@ -1,14 +1,16 @@
 import { Button, Form, Input } from "antd";
 import React, { useState } from "react";
 
-import { Encryption } from "../utils";
+import { Decryption, Encryption } from "../utils";
 
 const Password = () => {
-  const [EncryptionedPassword, setEncryptionedPassword] = useState("");
+  const [encryptedPassword, setEncryptedPassword] = useState("");
+  const [decryptedPassword, setDecryptedPassword] = useState("");
   const onFinish = async (values: any) => {
-    console.log("Success:", values);
-    const password = await Encryption("token", values.password);
-    setEncryptionedPassword(password);
+    const password = await Encryption(values.token, values.password);
+    const rawPassword = await Decryption(values.token, password);
+    setEncryptedPassword(password);
+    setDecryptedPassword(rawPassword);
   };
 
   const onFinishFailed = (errorInfo: any) => {
@@ -23,13 +25,14 @@ const Password = () => {
         paddingTop: 80,
       }}>
       <Form name="basic" initialValues={{ remember: true }} onFinish={onFinish} onFinishFailed={onFinishFailed}>
-        <Form.Item label="Session Token" name="username" rules={[{ required: true, message: "Please input your session token!" }]}>
+        <Form.Item label="Session Token" name="token" rules={[{ required: true, message: "Please input your session token!" }]}>
           <Input />
         </Form.Item>
         <Form.Item label="Password" name="password" rules={[{ required: true, message: "Please input your password!" }]}>
           <Input.Password />
         </Form.Item>
-        {EncryptionedPassword && <p>Encrypted Password: {EncryptionedPassword}</p>}
+        {encryptedPassword && <p>Encrypted Password: {encryptedPassword}</p>}
+        {decryptedPassword && <p>Decrypted Password: {decryptedPassword}</p>}
         <Form.Item>
           <Button type="primary" htmlType="submit">
             Submit
